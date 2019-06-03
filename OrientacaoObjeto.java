9e6cdcd1dbd115b7c66b5d1f00f6d884cccb00fb
Matheus Alves <47016580+MatheusMirro@users.noreply.github.com>
2019-06-03 16:03:05 -0300

    Orientação a objeto

---
+++ OrientacaoObjeto.java
@@ -0,0 +1,23 @@
+
+package orientacaoobjeto;
+
+
+public class OrientacaoObjeto {
+
+    
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 1;
+        c1.modelo = "Bic";
+        c1.status();
+        c1.tampar();
+        c1.rabiscar();
+        
+     
+        
+    }
+    
+}
